6ebeedaf161aa5c9cab4c27bfd391110f0931f79
Sakshi13Patil <sakshi.patil.che14@itbhu.ac.in>
2022-11-13 20:59:58 +0530

    initial commit

--- src/main/java/org/ledger/exception/LedgerException.java
+++ src/main/java/org/ledger/exception/LedgerException.java
@@ -1,10 +1,6 @@
 package org.ledger.exception;
 
 public class LedgerException extends RuntimeException {
-    public LedgerException() {
-        super();
-    }
-
     public LedgerException(String message) {
         super(message);
     }
